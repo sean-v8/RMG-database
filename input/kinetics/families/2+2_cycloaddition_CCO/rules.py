68a2f8c18cfe71e3a24cf2b371df7eaa22eef69c
Sean Troiano <stroiano7@gmail.com>
2012-08-27 18:00:46 -0400

    Removed 2+2_cycloaddition_CCO/rules/588
    Reference and kinetics match those of NIST entry at
    http://kinetics.nist.gov/kinetics/Detail?id=1972QUI/KNE61:1, but
    reactants do not contain a CCO group. Correct matching entry has
    been previously imported to 2+2_cycloaddition_Cd/NIST.

--- input/kinetics/families/2+2_cycloaddition_CCO/rules.py
+++ input/kinetics/families/2+2_cycloaddition_CCO/rules.py
@@ -8,34 +8,3 @@ longDesc = u"""
 """
 recommended = True
 
-entry(
-    index = 588,
-    label = "CCO;doublebond",
-    group1 = 
-"""
-1 *1 Cd  0 {2,D}
-2 *2 Cdd 0 {1,D} {3,D}
-3    Od  0 {2,D}
-""",
-    group2 = "OR{mb_CCO, mb_COC}",
-    kinetics = ArrheniusEP(
-        A = (6.92e+10,"cm^3/(mol*s)"),
-        n = 0,
-        alpha = 0,
-        E0 = (43.72,"kcal/mol"),
-        Tmin = (300,"K"),
-        Tmax = (1500,"K"),
-    ),
-    reference = None,
-    referenceType = "",
-    rank = 0,
-    shortDesc = u"""Quick et al. [107]""",
-    longDesc = 
-u"""
-
-""",
-    history = [
-        ("Mon Aug 27 14:48:27 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from the old RMG database."""),
-    ],
-)
-
