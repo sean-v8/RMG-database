88c8c1204aefbccc055f0a6c9349406f5af50967
Sean Troiano <stroiano7@gmail.com>
2012-08-27 18:07:00 -0400

    NIST Conversion: 2+2_cycloaddition_Cd/rules/586
    Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1972QUI/KNE61:1

--- input/kinetics/families/2+2_cycloaddition_Cd/rules.py
+++ input/kinetics/families/2+2_cycloaddition_Cd/rules.py
@@ -9,35 +9,6 @@ longDesc = u"""
 recommended = True
 
 entry(
-    index = 586,
-    label = "db;doublebond",
-    group1 = "OR{db_2H, db_HNd, db_HDe, db_Nd2, db_NdDe, db_De2}",
-    group2 = "OR{mb_db, mb_CO, mb_OC, mb_CCO, mb_COC}",
-    kinetics = ArrheniusEP(
-        A = (6.92e+10,"cm^3/(mol*s)"),
-        n = 0,
-        alpha = 0,
-        E0 = (43.72,"kcal/mol"),
-        Tmin = (723,"K"),
-        Tmax = (786,"K"),
-    ),
-    reference = None,
-    referenceType = "",
-    rank = 3,
-    shortDesc = u"""Quick et al. [107]""",
-    longDesc = 
-u"""
-[107] Quick, L. M. *Int. J. Chem. Kinet.* 1972, 4, 61. 
-
-C2H4 + C2H4 --> cyclobutane, absolute value measured directly using thermal excitation technique 
-and mass spectrometry. Pressure  0.40 - 1.73 bar.
-""",
-    history = [
-        ("Mon Aug 27 14:48:27 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from the old RMG database."""),
-    ],
-)
-
-entry(
     index = 6000,
     label = "db_2H;mb_OC",
     group1 = 
