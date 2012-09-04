cd968342ac3f989dd391f3ab5a56c4b120aad2c4
Sean Troiano <stroiano7@gmail.com>
2012-09-04 14:24:14 -0400

    NIST Conversion: Disproportionation/rules/541
    Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1987TSA471:32

--- input/kinetics/families/Disproportionation/NIST.py
+++ input/kinetics/families/Disproportionation/NIST.py
@@ -9547,3 +9547,59 @@ M. Harper, 9/1/09: Recommended A-factor and E/R parameter values (p. 22)
     ],
 )
 
+entry(
+    index = 162,
+    label = "1987TSA471:32",
+    reactant1 = 
+"""
+1 *3 C 1 {2,S}
+2 *2 O 0 {1,S} {3,S}
+3 *4 H 0 {2,S}
+""",
+    reactant2 = 
+"""
+1 *1 C 1
+""",
+    product1 = 
+"""
+1 *1 C 0 {2,S}
+2 *4 H 0 {1,S}
+""",
+    product2 = 
+"""
+1 *3 C 0 {2,D}
+2 *2 O 0 {1,D}
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (2.41e+06,"m^3/(mol*s)","*|/",5),
+        n = 0,
+        Ea = (0,"kJ/mol"),
+        T0 = 1,
+        Tmin = (300,"K"),
+        Tmax = (2500,"K"),
+    ),
+    reference = Article(
+        authors = ["Tsang, W."],
+        title = u'Chemical kinetic data base for combustion chemistry. Part 2. Methanol',
+        journal = "J. Phys. Chem. Ref. Data",
+        volume = "16",
+        pages = """471""",
+        year = "1987",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1987TSA471:32",
+    ),
+    referenceType = "review",
+    shortDesc = u"""Extensive literature review""",
+    longDesc = 
+u"""
+Uncertainty: 5.0
+
+===
+
+M. Harper, 8/30/09: No data at the time. Author estimates ratio of disproportionation rate to addition rate to be 0.2, namely 4E-12 cm3/molecule/s. (p. 500-501, Entry 39,16b)
+""",
+    history = [
+        ("Tue Sep  4 14:24:14 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1987TSA471:32"""),
+    ],
+)
+
--- input/kinetics/families/Disproportionation/rules.py
+++ input/kinetics/families/Disproportionation/rules.py
@@ -114,53 +114,6 @@ u"""
 )
 
 entry(
-    index = 541,
-    label = "C_methyl;O_Csrad",
-    group1 = 
-"""
-1 *1 C 1 {2,S} {3,S} {4,S}
-2    H 0 {1,S}
-3    H 0 {1,S}
-4    H 0 {1,S}
-""",
-    group2 = 
-"""
-1 *2 O  0 {2,S} {3,S}
-2 *3 Cs 1 {1,S}
-3 *4 H  0 {1,S}
-""",
-    kinetics = ArrheniusEP(
-        A = (2.41e+12,"cm^3/(mol*s)","*|/",5),
-        n = 0,
-        alpha = 0,
-        E0 = (0,"kcal/mol"),
-        Tmin = (300,"K"),
-        Tmax = (2500,"K"),
-    ),
-    reference = None,
-    referenceType = "",
-    rank = 4,
-    shortDesc = u"""Tsang [90] Literature review.""",
-    longDesc = 
-u"""
-[90] Tsang, W.; Journal of Physical and Chemical Reference Data (1987), 16(3), 471-508.
-Literature review: CH3 + CH2OH --> CH4 + CH2O
-
-pg. 500-501: Discussion on evaluated data
-
-Entry 39,16 (b): CH2OH + CH3 --> CH4 + CH2O
-
-Author estimates ratio of disproportionation rate to addition rate to be 0.2,
-
-namely 4x10^-12 cm3/molecule/s.  No data at the time.
-MRH 30-Aug-2009
-""",
-    history = [
-        ("Mon Aug 27 14:48:27 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from the old RMG database."""),
-    ],
-)
-
-entry(
     index = 542,
     label = "C_rad/H2/Cs;O_Csrad",
     group1 = 
