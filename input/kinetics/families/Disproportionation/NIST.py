7886a7563f7e3c9eca7de0cc6c9b25d9f0b984d3
Sean Troiano <stroiano7@gmail.com>
2012-09-04 15:04:36 -0400

    NIST Conversion: Disproportionation/rules/548
    Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1987TSA471:28

--- input/kinetics/families/Disproportionation/NIST.py
+++ input/kinetics/families/Disproportionation/NIST.py
@@ -9963,3 +9963,61 @@ M. Harper, 8/30/09: No data available at the time. Author suggests a disproporti
     ],
 )
 
+entry(
+    index = 169,
+    label = "1987TSA471:28",
+    reactant1 = 
+"""
+1    C 0 {2,T}
+2 *1 C 1 {1,T}
+""",
+    reactant2 = 
+"""
+1 *3 C 1 {2,S}
+2 *2 O 0 {1,S} {3,S}
+3 *4 H 0 {2,S}
+""",
+    product1 = 
+"""
+1 *1 C 0 {2,T} {3,S}
+2    C 0 {1,T}
+3 *4 H 0 {1,S}
+""",
+    product2 = 
+"""
+1 *3 C 0 {2,D}
+2 *2 O 0 {1,D}
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (3.61e+07,"m^3/(mol*s)","*|/",5),
+        n = 0,
+        Ea = (0,"kJ/mol"),
+        T0 = 1,
+        Tmin = (300,"K"),
+        Tmax = (2500,"K"),
+    ),
+    reference = Article(
+        authors = ["Tsang, W."],
+        title = u'Chemical kinetic data base for combustion chemistry. Part 2. Methanol',
+        journal = "J. Phys. Chem. Ref. Data",
+        volume = "16",
+        pages = """471""",
+        year = "1987",
+        url = "http://kinetics.nist.gov/kinetics/Detail?id=1987TSA471:28",
+    ),
+    referenceType = "review",
+    shortDesc = u"""Extensive literature review""",
+    longDesc = 
+u"""
+Uncertainty: 5.0
+
+===
+
+M. Harper, 8/30/09: No data available at the time. Author suggest a disproportionation rate coefficient of 6.0E-11 cm3/molecule/s, due to very exothermic rxn. (p. 504, Entry 39,21a)
+""",
+    history = [
+        ("Tue Sep  4 15:04:36 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from NIST database at http://kinetics.nist.gov/kinetics/Detail?id=1987TSA471:28"""),
+    ],
+)
+
--- input/kinetics/families/Disproportionation/rules.py
+++ input/kinetics/families/Disproportionation/rules.py
@@ -114,51 +114,6 @@ u"""
 )
 
 entry(
-    index = 548,
-    label = "Ct_rad;O_Csrad",
-    group1 = 
-"""
-1 *1 C 1 {2,T}
-2    C 0 {1,T}
-""",
-    group2 = 
-"""
-1 *2 O  0 {2,S} {3,S}
-2 *3 Cs 1 {1,S}
-3 *4 H  0 {1,S}
-""",
-    kinetics = ArrheniusEP(
-        A = (3.61e+13,"cm^3/(mol*s)","*|/",5),
-        n = 0,
-        alpha = 0,
-        E0 = (0,"kcal/mol"),
-        Tmin = (300,"K"),
-        Tmax = (2500,"K"),
-    ),
-    reference = None,
-    referenceType = "",
-    rank = 4,
-    shortDesc = u"""Tsang [90] Literature review.""",
-    longDesc = 
-u"""
-[90] Tsang, W.; Journal of Physical and Chemical Reference Data (1987), 16(3), 471-508.
-Literature review: C2H + CH2OH --> C2H2 + CH2O
-
-pg. 504: Discussion on evaluated data
-
-Entry 39,21 (a): CH2OH + C2H --> C2H2 + CH2O
-
-Author suggest a disproportionation rate coefficient of 6.0x10^-11 cm3/molecule/s, due
-
-to very exothermic rxn.  No data available at the time.
-MRH 30-Aug-2009
-""",
-    history = [
-        ("Mon Aug 27 14:48:27 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from the old RMG database."""),
-    ],
-)
-
-entry(
     index = 549,
     label = "CO_pri_rad;O_Csrad",
     group1 = 
