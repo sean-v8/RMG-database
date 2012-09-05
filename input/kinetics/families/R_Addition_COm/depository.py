3b603b02dbdaa9e09d010da124030e6abf9c5750
Sean Troiano <stroiano7@gmail.com>
2012-09-05 15:35:30 -0400

    Moved R_Addition_COm/rules/425 to depository

--- input/kinetics/families/R_Addition_COm/depository.py
+++ input/kinetics/families/R_Addition_COm/depository.py
@@ -49,3 +49,47 @@ CH3CO (doublet): EXTSYM = 1, one hindered rotor (methyl group, symmetry = 3)
     ],
 )
 
+entry(
+    index = 2,
+    reactant1 = 
+"""
+1    C 0 {2,S}
+2 *2 C 1 {1,S}
+""",
+    reactant2 = 
+"""
+1 *1 C 2T {2,D}
+2    O 0  {1,D}
+""",
+    product1 = 
+"""
+1 *2 C 0 {2,S} {3,S}
+2    C 0 {1,S}
+3 *1 C 1 {1,S} {4,D}
+4    O 0 {3,D}
+""",
+    degeneracy = 1,
+    kinetics = Arrhenius(
+        A = (77,"m^3/(mol*s)","*|/",3),
+        n = 1.37,
+        Ea = (23.807,"kJ/mol","+|-",8.368),
+        Tmin = (600,"K"),
+        Tmax = (2000,"K"),
+    ),
+    reference = None,
+    referenceType = "theory",
+    rank = 3,
+    shortDesc = u"""MRH CBS-QB3 calculations with 1dHR corrections""",
+    longDesc = 
+u"""
+Ethyl (doublet): external symmetry number (EXTSYM) = 1, one hindered rotor (methyl group, symmetry = 6)
+CO (singlet): EXTSYM = 1
+TS (doublet): EXTSYM = 1, two hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 1)
+CH3CH2CO (doublet): EXTSYM = 1, two hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 1)
+""",
+    history = [
+        ("Mon Aug 27 14:48:27 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from the old RMG database."""),
+        ("Wed Sep  5 15:31:37 2012","Sean Troiano <stroiano7@gmail.com>","action","""Moved from rules to depository."""),
+    ],
+)
+
--- input/kinetics/families/R_Addition_COm/rules.py
+++ input/kinetics/families/R_Addition_COm/rules.py
@@ -46,51 +46,6 @@ u"""
 )
 
 entry(
-    index = 425,
-    label = "COm;CH2CH3",
-    group1 = 
-"""
-1 *1 C {2S,2T} {2,D}
-2    O 0       {1,D}
-""",
-    group2 = 
-"""
-1 *2 C 1 {2,S} {3,S} {4,S}
-2    H 0 {1,S}
-3    H 0 {1,S}
-4    C 0 {1,S} {5,S} {6,S} {7,S}
-5    H 0 {4,S}
-6    H 0 {4,S}
-7    H 0 {4,S}
-""",
-    kinetics = ArrheniusEP(
-        A = (7.7e+07,"cm^3/(mol*s)","*|/",3),
-        n = 1.37,
-        alpha = 0,
-        E0 = (5.69,"kcal/mol","+|-",2),
-        Tmin = (600,"K"),
-        Tmax = (2000,"K"),
-    ),
-    reference = None,
-    referenceType = "",
-    rank = 3,
-    shortDesc = u"""MRH CBS-QB3 calculations with 1dHR corrections""",
-    longDesc = 
-u"""
-CH3CH2 + CO = CH3CH2CO
-MRH CBS-QB3 calculations with 1D hindered rotor corrections [MRHCBSQB31DHR]_.
-
-Ethyl (doublet): external symmetry number (EXTSYM) = 1, one hindered rotor (methyl group, symmetry = 6)
-CO (singlet): EXTSYM = 1
-TS (doublet): EXTSYM = 1, two hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 1)
-CH3CH2CO (doublet): EXTSYM = 1, two hindered rotors (methyl group, symmetry = 3; ethyl group, symmetry = 1)
-""",
-    history = [
-        ("Mon Aug 27 14:48:27 2012","Sean Troiano <stroiano7@gmail.com>","action","""Imported from the old RMG database."""),
-    ],
-)
-
-entry(
     index = 426,
     label = "COm;CH2CH2CH3",
     group1 = 
